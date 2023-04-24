c19cc8a5a295e0f62eb67dd4fd06661e55b5f810
Raman Kozar <pozitr0n.kozarroman@gmail.com>
2023-04-24 05:49:35 +0200

    Adding UITestField and changed TableViewController: added division into UIViewController, UITableViewDelegate, UITableViewDataSource and UITextFieldDelegate. Added all the constraints into main view controller

--- Hackaton-PJ-03-Flicker/Controller/CommonScreen/ImageTableViewController.swift
+++ Hackaton-PJ-03-Flicker/Controller/CommonScreen/ImageTableViewController.swift
@@ -7,8 +7,28 @@
 
 import UIKit
 
-class ImageTableViewController: UITableViewController {
+class ImageTableViewController: UIViewController {
 
+    let searchField: UITextField = {
+        
+        let searchField = UITextField()
+        
+        searchField.translatesAutoresizingMaskIntoConstraints = false
+        searchField.font = UIFont.systemFont(ofSize: 20)
+        searchField.backgroundColor = .gray
+        searchField.textColor = .white
+        searchField.borderStyle = UITextField.BorderStyle.roundedRect
+        searchField.autocorrectionType = UITextAutocorrectionType.no
+        searchField.keyboardType = UIKeyboardType.default
+        searchField.returnKeyType = UIReturnKeyType.done
+        searchField.clearButtonMode = UITextField.ViewMode.whileEditing
+        searchField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        searchField.placeholder = "What are you looking for?"
+        
+        return searchField
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,15 +46,84 @@ class ImageTableViewController: UITableViewController {
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        DispatchQueue.main.async {
+            self.navigationController?.pushViewController(ImageDetailsViewController(), animated: true)
+        }
+        
+    }
+    
+    func setupLayout() {
+        
+        view.backgroundColor = .lightGray
+        title = "All Flickers"
+        
+        let barHeight = view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        let tableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+       
+        tableView.backgroundColor = .lightGray
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+
+        tableView.register(ImageTableViewCell.self, forCellReuseIdentifier: ImageTableViewCell.identifier)
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        searchField.delegate = self
+        
+        view.addSubview(searchField)
+        view.addSubview(tableView)
+        
+        configureUI(tableView)
+        
+    }
+    
+    func configureUI(_ tableView: UITableView) {
+        
+        let margins = view.layoutMarginsGuide
+        
+        NSLayoutConstraint.activate([
+         
+            searchField.topAnchor.constraint(equalTo: margins.topAnchor, constant: 10.0),
+            searchField.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            searchField.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: searchField.bottomAnchor, constant: 20.0),
+            tableView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: margins.bottomAnchor)
+            
+        ])
+        
+    }
+    
+}
+
+extension ImageTableViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        searchField.resignFirstResponder()
+        return true
+        
+    }
+
+}
+
+extension ImageTableViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ImageTableViewCell.identifier, for: indexPath) as? ImageTableViewCell
         else {
@@ -44,17 +133,6 @@ class ImageTableViewController: UITableViewController {
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        DispatchQueue.main.async {
-            self.navigationController?.pushViewController(ImageDetailsViewController(), animated: true)
-        }
-        
-    }
-    
-    func setupLayout() {
-        view.backgroundColor = .lightGray
-        tableView.register(ImageTableViewCell.self, forCellReuseIdentifier: ImageTableViewCell.identifier)
-    }
-    
 }
+
+
